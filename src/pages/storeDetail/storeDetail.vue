0a20a8947e2d8ca81145fa4f7c2b8a41e96bf63f
董文浩 <dwhandwp@163.com>
2019-03-18 19:18:17 +0800

    ‘‘。。’

--- src/pages/storeDetail/storeDetail.vue
+++ src/pages/storeDetail/storeDetail.vue
@@ -16,6 +16,20 @@
         </swiper-item>
       </div>
     </swiper>
+    <div class="address">
+      <h4>地址</h4>
+      <div class="store">$店名$$距离$</div>
+      <div class="address-detail">$一个很长很长很长很长很长很长很长的地址$</div> 
+    </div>
+    <div class="business-hours">
+      <div class="business">
+        <div class="business-text">营业时间</div>
+        <div class="time">$09:00-22:00$</div>
+      </div>
+      <div class="phone">
+        <img src="/static/images/icon-phone.png">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -37,6 +51,55 @@ export default {
   .banner {
     width: 100%;
     height: 100%;
+  }
+  .address {
+    padding: 12px 20px;
+    border-bottom: 1px solid #eee;
+    >h4 {
+      font-weight: bold;
+      font-size: 16px;
+    }
+    .store {
+      font-size: 16px;
+      color: @theme-color;
+      margin: 8px 0;
+    }
+    .address-detail {
+      color: #bababa;
+    }
+  }
+  .business-hours {
+    display: flex;
+    height: 75px;
+    border-bottom: 1px solid #eee;
+    .business {
+      flex: 1;
+      padding-left: 20px;
+      .business-text {
+        font-size: 16px;
+        margin-top: 12px;
+        font-weight: bold;
+      }
+      .time {
+        margin-top: 10px;
+        color: #bababa;
+      }
+    }
+    .phone {
+      flex: 0 0 75px;
+      position: relative;
+      box-sizing: border-box;
+      >img {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        padding: 12px 18px;
+        width: 30px;
+        height: 30px;
+        border-left: 1px solid #eee;
+      }
+    }
   }
 }
 </style>
